b83edb3caec7920a73daa9179dcbd5134f080748
Sthealth <AW@example.com>
2025-10-09 22:43:21 -0400

    add constructor, gettors, and tostring to transaction class

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,42 @@
 package com.pluralsight;
 
 public class Transaction {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double price;
+
+    public Transaction(String date, String time, String description, String vendor, double price) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.price = price;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return this.date + "     " + this.time + "     " + this.description + "    " + this.vendor + "        " + this.price;
+    }
 }
